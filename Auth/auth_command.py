4faebd806ad639e3b5e5eeb6c676623959e45147
root <root@kevin.EE4215.local>
2019-04-16 01:15:40 +0800

    hox fix for some issue

--- Auth/auth_command.py
+++ Auth/auth_command.py
@@ -1,8 +1,10 @@
 import sys
 import os
+import time
 input_arg = sys.argv
 addpeer_ip = input_arg[1]
 commandA = ("curl -H Content-type:application/json --data '{\"peer\" : \"ws://%s:6001\"}' http://localhost:3003/addPeer"%addpeer_ip)
 commandB = "curl http://127.0.0.1:3003/authall"
 os.system(commandA)
 os.system(commandB)
+time.sleep(1)
